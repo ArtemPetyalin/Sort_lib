1f9893a54c9e60c61a54a9e146011f6ae9d7a3c3
Петялин Артём Дмитриевич <a.petyalin@main.nsu.ru>
2024-11-23 14:50:18 +0700

    остаются 2 элемента, которые не сортируются

--- main.py
+++ main.py
@@ -46,13 +46,13 @@ def quick_sort(l):
     if len(l) <= 1:
         return l
 
-    indexer = []
-    counter = 1
     def sorter(listofor):
         import random
 
         if len(listofor) > 1:
-            a = random.choice(listofor)
+            ind_a = random.randint(0, len(listofor) - 1)
+            a = listofor[ind_a]
+            listofor.pop(ind_a)
             less_arr = []
             bigger_arr = []
             for i in range(len(listofor)):
@@ -63,15 +63,43 @@ def quick_sort(l):
                 else:
                     less_arr.append(listofor[i])
 
-            less_arr.append(a)
-            result = less_arr + bigger_arr
-            less_arr.pop(-1)
 
-            index_inside = len(listofor) - len(bigger_arr)
+            listofor = less_arr + [str(a) + 's'] + bigger_arr
 
-        return [a, index_inside]
+        return listofor
 
-    for i in range(len(l) // 2 + 1):
+    for i in range(len(l)):
+        counter = 0
+        index1 = 0
+        print('run')
+        for j in range(len(l)):
+
+            if str(l[j])[-1] == 's' and j != index1:
+
+                if j - index1 > 2:
+                    l = l[:index1 + 1] + sorter(l[index1 + 1:j]) + l[j:]
+                    counter += 1
+                    index1 = j
+                    print(l)
+
+                if j - index1 == 2:
+                    l[j - 1] = str(l[j - 1]) + 's'
+                    index1 = j
+                    counter += 1
+
+                elif j - index1 == 1:
+                     index1 = j
+                     counter += 1
+
+            elif j == len(l) - 1 and str(l[j]) != 's':
+                l = l[:index1 + 1] + sorter(l[index1 + 1:])
+                print(l)
+
+    return l
+
+print([5,4,1,2,8,9,0,414,13,11,54,52,61,87])
+print(quick_sort([5,4,1,2,8,9,0,414,13,11,54,52,61,87]))
+
 
 
 
